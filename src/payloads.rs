7a902707d74a77366353a7b6017cfa3291a793ac
brendanglancy <bglance68@gmail.com>
2024-05-31 20:46:55 -0400

    we get through the file

--- src/payloads.rs
+++ src/payloads.rs
@@ -1,5 +1,4 @@
 pub mod payloads {
-    use std::convert::TryInto;
     use std::fs::File;
     use std::io::{BufReader, Read};
     use std::path::PathBuf;
@@ -11,34 +10,34 @@ pub mod payloads {
         file_path.push("nmap-payloads");
 
         let mut data = String::new();
-        let f = File::open(&file_path).expect("File not found.");
-        let mut file_reader = BufReader::new(f);
+        let file = File::open(&file_path).expect("File not found.");
+        let mut file_reader = BufReader::new(file);
         file_reader
             .read_to_string(&mut data)
             .expect("Unable to read file.");
 
         for line in data.split("\n") {
-            if line.trim_start().starts_with("#") {
-                continue;
-            }
-            if line.starts_with("udp") {
-                let rest = &line[4..];
-                let mut parts = rest.split(" ");
-                let ports = parts.next().unwrap();
-                let ports = get_ports_from_line(ports);
-                for port in ports {
-                    println!("Port: {}", port);
-                }
+            match line.trim_start().chars().next() {
+                Some('#') => continue,
+                Some('u') => {
+                    let rest = &line[4..];
+                    let mut parts = rest.split(" ");
+                    let ports = parts.next().unwrap();
+                    let ports = get_ports_from_line(ports);
+                    for port in ports {
+                        println!("Port: {}", port);
+                    }
 
-                if let Some(_) = parts.next() {
+                    if let Some(_) = parts.next() {
+                        let payload = get_payload_from_line(line);
+                        println!("{}", payload);
+                    }
+                }
+                Some(' ') => {
                     let payload = get_payload_from_line(line);
                     println!("{}", payload);
                 }
-            }
-
-            if line.starts_with("  ") {
-                let payload = get_payload_from_line(line);
-                println!("{}", payload);
+                _ => {}
             }
         }
     }
@@ -46,25 +45,32 @@ pub mod payloads {
     pub fn get_payload_from_line(line: &str) -> String {
         let start = line.find("\"").expect("No opening quote found.");
         let end = line.rfind("\"").expect("No closing quote found.");
+        println!("start {}", start);
 
-        line[start + 1..end].to_string()
+        line[start..end].to_string()
     }
 
     pub fn get_ports_from_line(ports: &str) -> Vec<u16> {
         let port_segments: Vec<&str> = ports.split(",").collect();
         let mut port_list: Vec<u16> = Vec::new();
+
         for segment in port_segments {
+
             if segment.contains("-") {
                 let range: Vec<&str> = segment.trim().split("-").collect();
                 let start = range[0].parse::<u16>().unwrap();
                 let end = range[1].parse::<u16>().unwrap();
-                for port in start..end + 1 {
+                println!("start {} end {}", start, end);
+
+                for port in start..end {
                     port_list.push(port);
                 }
+
             } else if !segment.is_empty() {
                 let int: u16 = segment.parse().unwrap();
                 port_list.push(int);
             }
+
         }
         port_list
     }
