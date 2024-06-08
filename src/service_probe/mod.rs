541cacc8113a9a624bc4990474b9f2e1643e98f3
brendanglancy <bglance68@gmail.com>
2024-06-07 20:37:22 -0400

    parsing almost there I think

--- src/service_probe/mod.rs
+++ src/service_probe/mod.rs
@@ -1,9 +1,9 @@
-use std::collections::{BTreeMap, HashMap, HashSet};
+use std::collections::BTreeMap;
 use std::fs::File;
 
 use std::io::{BufReader, Read};
 use std::path::PathBuf;
-use std::{env, i32, mem, u16, u8};
+use std::{env, i32, u16};
 
 pub fn file_to_map() {
     let current_dir = env::current_dir().expect("cant find curr dir");
@@ -23,7 +23,6 @@ fn f_btree(mut file_buf: BufReader<File>, mut data: String) {
         .expect("unable to read file");
 
     let mut fp_map: BTreeMap<i32, String> = BTreeMap::new();
-    let mut f_map: BTreeMap<i32, String> = BTreeMap::new();
 
     let mut count = 0;
     let mut capturing = false;
@@ -45,64 +44,80 @@ fn f_btree(mut file_buf: BufReader<File>, mut data: String) {
 
         if capturing {
             if !curr.is_empty() {
-                // split on new lines with a space
-                // curr.push(' ');
+                curr.push(' ');
             }
             curr.push_str(line);
         }
     }
 
-    for (line_nr, data) in fp_map {
-        println!("{} {}", line_nr, data);
+    let pb_linenr = ports_v(&fp_map);
+    let payb_linenr = payloads_v(&fp_map);
+
+    for (line_nr, ports) in pb_linenr {
+        println!("{} {:?}", line_nr, ports);
+    }
+
+    for (line_nr, payloads) in payb_linenr {
+        println!("{} {:?}", line_nr, payloads);
     }
 }
 
-fn ports_v(line: String) -> Vec<u16> {
+fn ports_v(fp_map: &BTreeMap<i32, String>) -> BTreeMap<i32, Vec<u16>> {
+    let mut pb_linenr: BTreeMap<i32, Vec<u16>> = BTreeMap::new();
     let mut port_list: Vec<u16> = Vec::new();
 
-    // we already check this
-    if line.contains("udp ") {
-        let remain = &line[4..];
-        let mut start = remain.split(" ");
+    for (&line_nr, ports) in fp_map {
+        if ports.contains("udp ") {
+            let remain = &ports[4..];
+            let mut start = remain.split(" ");
 
-        let ports = start.next().unwrap();
-        let port_segments: Vec<&str> = ports.split(",").collect();
+            let ports = start.next().unwrap();
+            let port_segments: Vec<&str> = ports.split(",").collect();
 
-        for segment in port_segments {
-            if segment.contains("-") {
-                let range: Vec<&str> = segment.trim().split("-").collect();
-                let start = range[0].parse::<u16>().unwrap();
-                let end = range[1].parse::<u16>().unwrap();
+            for segment in port_segments {
+                if segment.contains("-") {
+                    let range: Vec<&str> = segment.trim().split("-").collect();
+                    let start = range[0].parse::<u16>().unwrap();
+                    let end = range[1].parse::<u16>().unwrap();
 
-                for port in start..end {
+                    for port in start..end {
+                        port_list.push(port);
+                    }
+                } else if !segment.is_empty() {
+                    let port: u16 = segment.parse().unwrap();
                     port_list.push(port);
                 }
-            } else if !segment.is_empty() {
-                let port: u16 = segment.parse().unwrap();
-                port_list.push(port);
             }
         }
+
+        pb_linenr.insert(line_nr, port_list.clone());
+        port_list.clear();
     }
 
-    port_list
+    pb_linenr
 }
 
-fn payloads_v(payloadp: &Vec<String>) -> Vec<u8> {
-    let mut payloads: Vec<u8> = Vec::new();
+fn payloads_v(fp_map: &BTreeMap<i32, String>) -> BTreeMap<i32, Vec<Vec<String>>> {
+    let mut payb_linenr: BTreeMap<i32, Vec<Vec<String>>> = BTreeMap::new();
+    let mut ploads: Vec<Vec<String>> = Vec::new();
+    let mut sin_pload: Vec<String> = Vec::new();
 
-    for payload in payloadp {
-        payloads = payload.as_bytes().to_vec();
+    for (&line_nr, data) in fp_map {
+        if data.contains("\"") {
+            let start = data.find("\"").expect("payload opening \" not found");
+            let end = data.rfind("\"").expect("payload closing \" not found");
+
+            let payloads = data[start..end].split(" ");
+            for payload in payloads {
+                sin_pload.push(payload.to_string());
+                ploads.push(sin_pload.to_vec());
+                sin_pload.clear();
+            }
+        }
+
+        payb_linenr.insert(line_nr, ploads.to_vec());
+        ploads.clear();
     }
 
-    payloads
-}
-
-fn init_map(combos: &Vec<String>) -> HashMap<Vec<u16>, Vec<u8>> {
-    let mut map: HashMap<Vec<u16>, Vec<u8>> = HashMap::new();
-
-    let port_v = ports_v(&combos.to_vec());
-    let payload_v = payloads_v(&combos.to_vec());
-    map.insert(port_v, payload_v);
-
-    map
+    payb_linenr
 }
